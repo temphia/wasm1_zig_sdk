8b6b8f8899007a90a7ff4bf14cc0170c4f6ff48a
bornjre <bornjre@gmail.com>
2023-05-07 04:07:30 +0545

    implement custom logger

--- example/main.zig
+++ example/main.zig
@@ -2,20 +2,27 @@ const std = @import("std");
 const sdk = @import("wasm1-zig-sdk");
 const core = sdk.Core;
 
+ const allocator = std.heap.page_allocator;
+
 pub const std_options = struct {
-    pub fn logFn(
-        comptime _: std.log.Level,
-        comptime _: @Type(.EnumLiteral),
-        comptime _: []const u8,
-        _: anytype,
-    ) void {}
+    pub fn logFn(comptime lvl: std.log.Level, comptime _: @Type(.EnumLiteral), comptime msg: []const u8, args: anytype) void {
+        var buffer = std.ArrayList(u8).init(allocator);
+
+        std.fmt.format(buffer.writer(), "{s} {s} {any}\n", .{ lvl.asText(), msg, args }) catch unreachable;
+        core.log(buffer.items);
+
+        buffer.deinit();
+    }
 };
 
 pub export fn action_hello(ptr: i32, size: i32) void {
     _ = ptr;
     _ = size;
-    
+
     core.log("HEYYYYY");
 
-    // _ = sdk.utils.stringToPtr("Hello");
+
+
+    // std.debug.print("HEY", .{});
+
 }
--- src/lib.zig
+++ src/lib.zig
@@ -1,6 +1,6 @@
 const core = @import("./core.zig");
-const utils = @import("./utils.zig");
 
+pub const utils = @import("./utils.zig");
 pub const Core = core.Core;
 
 const raw = .{ .core = core, .utils = utils };
