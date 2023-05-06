717c20d9dba9fd1b6ab05d8d1bd37d70867e0402
bornjre <bornjre@gmail.com>
2023-05-06 23:48:10 +0545

    fix log error by defining a std log options

--- example/main.zig
+++ example/main.zig
@@ -2,6 +2,16 @@ const std = @import("std");
 const sdk = @import("wasm1-zig-sdk");
 const core = sdk.core;
 
+pub const std_options = struct {
+    pub fn logFn(
+        comptime _: std.log.Level,
+        comptime _: @Type(.EnumLiteral),
+        comptime _: []const u8,
+        _: anytype,
+    ) void {}
+};
+
 pub export fn action_hello(ptr: i32, size: i32) void {
     core.log(ptr + 42, size);
+    _ = sdk.utils.stringToPtr("Hello");
 }
