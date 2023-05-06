9ff1f80f8064a3f403da1871e5566574c8c57ea6
bornjre <bornjre@gmail.com>
2023-05-06 07:06:37 +0545

    fix import as module

--- build.zig
+++ build.zig
@@ -12,8 +12,6 @@ pub fn build(b: *Builder) void {
 
     target.os_tag = .freestanding;
 
-    b.addPackagePath("wasm1-zig-sdk", "src");
-
     const wlib = b.addStaticLibrary(.{
         .name = "wasm_zig_sdk",
         .target = target,
@@ -21,10 +19,14 @@ pub fn build(b: *Builder) void {
         .root_source_file = .{ .path = "example/main.zig" },
     });
 
+    wlib.addAnonymousModule("wasm1-zig-sdk", .{
+        .source_file = .{ .path = "src/lib.zig" },
+    });
+
     wlib.force_pic = true;
 
 
-    wlib.addPackage("wasm1-zig-sdk");
+    // wlib.addPackage("wasm1-zig-sdk");
 
     b.installArtifact(wlib);
 }
--- example/main.zig
+++ example/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const core = @import("wasm1-zig-sdk");
+const core = @import("wasm1-zig-sdk").core;
 
 export fn action_hello(ptr: i32, size: i32) void {
     core.log(ptr, size);
