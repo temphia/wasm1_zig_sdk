004ea5444949cb42a07b309c0fe1b0764faa9782
bornjre <bornjre@gmail.com>
2023-05-06 06:15:19 +0545

    build.zig wip and add phony to make

--- build.zig
+++ build.zig
@@ -1,16 +1,25 @@
 const std = @import("std");
+const fs = std.fs;
+const Builder = std.build.Builder;
+const LibExeObjStep = std.build.LibExeObjStep;
+const CrossTarget = std.zig.CrossTarget;
+const Mode = std.builtin.Mode;
+const builtin = @import("builtin");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *Builder) void {
+    var target = b.standardTargetOptions(.{});
+    const mode = b.standardOptimizeOption(.{});
 
-    const mode = b.standardReleaseOptions();
+    target.os_tag = .freestanding;
 
-    const lib = b.addStaticLibrary("wasm1_zig_sdk", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const wlib = b.addStaticLibrary(.{
+        .name = "wasm_zig_sdk",
+        .target = target,
+        .optimize = mode,
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
 
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    wlib.force_pic = true;
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    b.installArtifact(wlib);
 }
--- makefile
+++ makefile
@@ -1,7 +1,9 @@
+.PHONY: build build_zip
+
 build:
 	zig build-lib src/main.zig -target wasm32-freestanding -dynamic -OReleaseSmall
-	mv main.wasm zig-out/
-	mv main.wasm.o zig-out/
-
+	mv main.wasm zig-cache/bin
+	mv main.wasm.o zig-cache/bin
+	wasm2wat zig-cache/bin/main.wasm -o zig-cache/bin/main.wat
 build_zip:
 	TEMPHIA_BDEV_BPRINT_CONFIG="src/example/bprint.yaml" temphia-cli bdev zip
