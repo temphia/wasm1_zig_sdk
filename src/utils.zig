66cd684d8292f2c59dc0076bb748a2202e186b65
bornjre <bornjre@gmail.com>
2023-05-06 23:54:11 +0545

    define utils funcs to alloc and free bytes and convert to and from string

--- src/utils.zig
+++ src/utils.zig
@@ -1,11 +1,26 @@
 const std = @import("std");
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa = general_purpose_allocator.allocator();
 
-pub export fn allocBytes(size: i32) [*]u8 {
-    return gpa.alloc(u8, size) catch unreachable;
+pub fn getSDKAllocator() *std.mem.Allocator {
+    return gpa;
 }
 
-pub export fn freeBytes(addr: i32) void {
-    gpa.destroy(addr);
+pub export fn allocBytes(size: usize) usize {
+    const baddr = gpa.alloc(u8, size) catch unreachable;
+    return @ptrToInt(&baddr[0]);
+}
+
+pub export fn freeBytes(addr: usize) void {
+    gpa.destroy(@intToPtr([*]u8, addr));
+}
+
+pub fn stringToPtr(s: []const u8) struct { ptr: usize, len: usize } {
+    const sptr = &s[0];
+
+    return .{
+        .ptr = @ptrToInt(sptr),
+        .len = s.len,
+    };
 }
