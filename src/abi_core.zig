238b50bacbe85d97499977f040ea0325cdab629c
bornjre <bornjre@gmail.com>
2023-06-03 07:30:04 +0545

    format/refactor

--- src/abi_core.zig
+++ src/abi_core.zig
@@ -2,22 +2,6 @@ const std = @import("std");
 const utils = @import("./utils.zig");
 const Result = @import("./result.zig").Result;
 
-extern "temphia1" fn log(ptr: usize, size: usize) void;
-extern "temphia1" fn sleep(msec: u32) void;
-extern "temphia1" fn get_self_file(ctxid: usize, fptr: usize, fsize: usize, respptr: usize, resplen: usize, modPtr: usize) void;
-
-pub inline fn raw_log(ptr: usize, size: usize) void {
-    log(ptr, size);
-}
-
-pub inline fn raw_sleep(msec: u32) void {
-    sleep(msec);
-}
-
-pub inline fn raw_get_self_file(fptr: usize, fsize: usize, respptr: usize, resplen: usize, modPtr: usize) void {
-    get_self_file(fptr, fsize, respptr, resplen, modPtr);
-}
-
 pub const Core = struct {
     pub fn log(s: []const u8) void {
         const resp = utils.stringToPtr(s);
@@ -49,3 +33,23 @@ pub const Core = struct {
         return Result.fromErrBytes(utils.ptrToBytes(rPtr, rSize));
     }
 };
+
+// raw
+
+pub inline fn raw_log(ptr: usize, size: usize) void {
+    log(ptr, size);
+}
+
+pub inline fn raw_sleep(msec: u32) void {
+    sleep(msec);
+}
+
+pub inline fn raw_get_self_file(fptr: usize, fsize: usize, respptr: usize, resplen: usize, modPtr: usize) void {
+    get_self_file(fptr, fsize, respptr, resplen, modPtr);
+}
+
+// abi
+
+extern "temphia1" fn log(ptr: usize, size: usize) void;
+extern "temphia1" fn sleep(msec: u32) void;
+extern "temphia1" fn get_self_file(ctxid: usize, fptr: usize, fsize: usize, respptr: usize, resplen: usize, modPtr: usize) void;
--- src/abi_self.zig
+++ src/abi_self.zig
@@ -3,21 +3,20 @@ const utils = @import("./utils");
 const result = @import("./result.zig");
 const Result = result.Result;
 
-extern "temphia1" fn self_list_resources(ctxid: usize, respOffset: usize, respLen: usize) bool;
+pub const Self = struct {
+    pub fn ListResources() Result {
+        const roffset: usize = 0;
+        const rlen: usize = 0;
 
-extern "temphia1" fn self_get_resource(ctxid: usize, nPtr: usize, nLen: usize, respOffset: usize, respLen: usize) bool;
+        if (raw_self_list_resources(0, @ptrToInt(&roffset), @ptrToInt(&rlen))) {
+            return result.fromErrBytes(utils.ptrToBytes(roffset, rlen));
+        }
 
-extern "temphia1" fn self_in_links(ctxid: usize, respOffset: usize, respLen: usize) bool;
+        return result.fromErrBytes(utils.ptrToBytes(roffset, rlen));
+    }
+};
 
-extern "temphia1" fn self_out_links(ctxid: usize, respOffset: usize, respLen: usize) bool;
-
-extern "temphia1" fn self_link_exec(ctxid: usize, nPtr: usize, nLen: usize, mPtr: usize, mLen: usize, dPtr: usize, dLen: usize, asynk: usize, detached: usize, respOffset: usize, respLen: usize) bool;
-
-extern "temphia1" fn self_new_module(ctxid: usize, nPtr: usize, nLen: usize, dPtr: usize, dLen: usize, respOffset: usize, respLen: usize) usize;
-
-extern "temphia1" fn self_module_exec(ctxid: usize, mid: usize, mPtr: usize, mLen: usize, dPtr: usize, dLen: usize, respOffset: usize, respLen: usize) bool;
-
-extern "temphia1" fn self_fork_exec(ctxid: usize, mPtr: usize, mLen: usize, dPtr: usize, dLen: usize, respOffset: usize, respLen: usize) bool;
+// raw
 
 pub inline fn raw_self_list_resources(ctxid: usize, respOffset: usize, respLen: usize) bool {
     return self_list_resources(respOffset, respLen);
@@ -51,15 +50,13 @@ pub inline fn raw_self_fork_exec(ctxid: usize, mPtr: usize, mLen: usize, dPtr: u
     return self_fork_exec(mPtr, mLen, dPtr, dLen, respOffset, respLen);
 }
 
-pub const Self = struct {
-    pub fn ListResources() Result {
-        const roffset: usize = 0;
-        const rlen: usize = 0;
+// abi
 
-        if (raw_self_list_resources(0, @ptrToInt(&roffset), @ptrToInt(&rlen))) {
-            return result.fromErrBytes(utils.ptrToBytes(roffset, rlen));
-        }
-
-        return result.fromErrBytes(utils.ptrToBytes(roffset, rlen));
-    }
-};
+extern "temphia1" fn self_list_resources(ctxid: usize, respOffset: usize, respLen: usize) bool;
+extern "temphia1" fn self_get_resource(ctxid: usize, nPtr: usize, nLen: usize, respOffset: usize, respLen: usize) bool;
+extern "temphia1" fn self_in_links(ctxid: usize, respOffset: usize, respLen: usize) bool;
+extern "temphia1" fn self_out_links(ctxid: usize, respOffset: usize, respLen: usize) bool;
+extern "temphia1" fn self_link_exec(ctxid: usize, nPtr: usize, nLen: usize, mPtr: usize, mLen: usize, dPtr: usize, dLen: usize, asynk: usize, detached: usize, respOffset: usize, respLen: usize) bool;
+extern "temphia1" fn self_new_module(ctxid: usize, nPtr: usize, nLen: usize, dPtr: usize, dLen: usize, respOffset: usize, respLen: usize) usize;
+extern "temphia1" fn self_module_exec(ctxid: usize, mid: usize, mPtr: usize, mLen: usize, dPtr: usize, dLen: usize, respOffset: usize, respLen: usize) bool;
+extern "temphia1" fn self_fork_exec(ctxid: usize, mPtr: usize, mLen: usize, dPtr: usize, dLen: usize, respOffset: usize, respLen: usize) bool;
